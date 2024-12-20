61a87501a1c0a079d30dd824ddeb2e2469f99d0e
urcosmos <urcosmos@yandex.ru>
2024-12-20 13:51:56 +0300

    Commit with error. Insert this lint with git commit --amend

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ testing git commit -a
 
 let a = 10;
 console.log(a);
+
+console.log('Случайная ошибка, которая пошла в коммит');
+console.log('А это исправление ошибки в коммите');
+console.log('И еще раз');
